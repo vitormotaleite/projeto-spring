0c3f1836b164d1a51193aa20ff6013eaaaa8a80e
vitormotaleite <vitor.mota.diniz@ccc.ufcg.edu.br>
2022-09-05 12:33:10 -0300

    projeto iniciado

---
+++ src/main/java/com/vitor/spring/ProjetoSbApplication.java
@@ -0,0 +1,13 @@
+package com.vitor.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSbApplication.class, args);
+	}
+
+}
