b4e48742d3fd156a08c63b4e6b5c69145b03790c
vitormotaleite <vitor.mota.diniz@ccc.ufcg.edu.br>
2022-09-08 13:27:22 -0300

    payment

--- src/main/java/com/vitor/spring/config/TestConfig.java
+++ src/main/java/com/vitor/spring/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.vitor.spring.entities.Category;
 import com.vitor.spring.entities.Order;
 import com.vitor.spring.entities.OrderItem;
+import com.vitor.spring.entities.Payment;
 import com.vitor.spring.entities.Product;
 import com.vitor.spring.entities.User;
 import com.vitor.spring.entities.enums.OrderStatus;
@@ -80,5 +81,9 @@ public class TestConfig implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
 		
 		this.orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
+		
+		Payment pay1 = new Payment(null,Instant.parse("2019-06-20T21:53:07Z"),o1);
+		o1.setPayment(pay1);
+		this.orderRepository.save(o1);
 	}
 }
