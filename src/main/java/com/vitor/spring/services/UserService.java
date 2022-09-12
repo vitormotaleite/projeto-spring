ebeb47ca5b5eb238e0cd7f32cbb6f70a84c9ddcf
vitormotaleite <vitor.mota.diniz@ccc.ufcg.edu.br>
2022-09-12 09:37:47 -0300

    excecao delete

--- src/main/java/com/vitor/spring/services/UserService.java
+++ src/main/java/com/vitor/spring/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.vitor.spring.entities.User;
 import com.vitor.spring.repositories.UserRepository;
+import com.vitor.spring.services.exceptions.DatabaseException;
 import com.vitor.spring.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -30,7 +33,13 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	
 	@SuppressWarnings("deprecation")
